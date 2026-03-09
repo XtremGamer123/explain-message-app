96d66ac06b44f3016588cbfaa2277a601732cfbe
XtremGamer <83528105+XtremGamer123@users.noreply.github.com>
2026-03-09 16:56:34 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:"Arial"}}>
+      <h1>India Helper App</h1>
+      <p>Your app is live 🎉</p>
+      <p>This app helps people find help and services nearby.</p>
+    </div>
+  );
+}
