a85d13299a32eb30ddbc2bf8a6e77dfb7b04ce49
XtremGamer <83528105+XtremGamer123@users.noreply.github.com>
2026-03-09 18:06:30 +0530

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:40,fontFamily:"Arial"}}>
-      <h1>India Helper App</h1>
-      <p>Your app is live 🎉</p>
-      <p>This app helps people find help and services nearby.</p>
-    </div>
-  );
-}
